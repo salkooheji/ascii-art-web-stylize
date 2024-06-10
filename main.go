4eb831d788c682d13ab2be071513bcf813349696
hamonia <haithammonia3>
2024-06-10 04:16:52 +0300

    GET and Post Done

--- main.go
+++ main.go
@@ -13,33 +13,55 @@ type output struct {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err%v", err)
-		return
-	}
-
+	//Data needed for the templates
 	text := r.FormValue("text")
 	fmt.Println(text)
 	font := r.FormValue("font")
 	result := functions.FinalResult(text, font)
 
-	// Parse the template file
-	tmpl, err := template.ParseFiles("static/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	if r.Method == "GET" {
+		// Parse the template file
+		tmpl, err := template.ParseFiles("static/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
 
-	// Create a data structure to pass to the template
-	data := output{
-		message: result,
-	}
+		// Create a data structure to pass to the template
+		data := output{
+			message: result,
+		}
 
-	// Execute the template with the data
-	err = tmpl.Execute(w, data.message)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		// Execute the template with the data
+		err = tmpl.Execute(w, data.message)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err%v", err)
+			return
+		}
+
+		// Parse the template file
+		tmpl, err := template.ParseFiles("static/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Create a data structure to pass to the template
+		data := output{
+			message: result,
+		}
+
+		// Execute the template with the data
+		err = tmpl.Execute(w, data.message)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
@@ -56,8 +78,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// This for the GET the server directs to the index to html
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	//This for handling the POST request from the form
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 	fmt.Printf("Starting server at port 8080\n")
