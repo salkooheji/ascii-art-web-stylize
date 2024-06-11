b3d91e412a2e912b06f432283b493248a05d4aa5
hamonia <haithammonia3>
2024-06-11 03:00:00 +0300

    Errors Left stylize

--- functions/finalResult.go
+++ functions/finalResult.go
@@ -3,6 +3,7 @@ package Ascii
 import "strings"
 
 func FinalResult(text string, font string) string {
+	font = "fonts/" + font
 	shapeFileContent := ReadShapeFile(font)
 	charactersMap := MapFileContentToRunes(shapeFileContent)
 	textToDrawSplit := strings.Split(text, "\n")
